a7a036445e25526a84469de09acac1772d92f5c9
Dimar Hanung <dimarhanung@pop-os.localdomain>
2021-04-28 21:10:29 +0700

    chore(delete): wrong file place

--- hello-world.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function helloWorld(num) {
-  return "Hello World " + num;
-}
-var hello1 = helloWorld(1);
-var hello2 = helloWorld(2);
-console.log({ hello1: hello1, hello2: hello2 });
--- hello-world.ts
+++
@@ -1,9 +0,0 @@
-function helloWorld(num: number): string {
-  return "Hello World " + num;
-}
-
-const hello1: string = helloWorld(1);
-const hello2: string = helloWorld(2);
-
-console.log({ hello1, hello2 });
-export {};
